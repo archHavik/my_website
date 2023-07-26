c630dbcf98979552aa58e1b0c89bf577e07cf25b
archHavik <havik222@gmail.com>
2023-07-25 21:59:45 -0600

    fixed href to favicon.ico

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,7 @@ export default function Layout({ children, color }) {
   return (
     <div>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
         <meta name="og:title" content={siteTitle} />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       </Head>
